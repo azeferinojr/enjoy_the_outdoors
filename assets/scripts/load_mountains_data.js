3445e07756a102f91ccbcbb03d91f22229487943
Zeferino Jr <Antonio.ZeferinoJr@libertymutual.com>
2022-07-01 06:02:25 -0700

    Cleaned up my template and replaced template text with actual text that I want visitors to see. This applies to all template pages.

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -2,10 +2,24 @@
 
 let mountainsArray = []
 
+//added this
+let mountainsDDL = document.querySelector("#mountainsDDL"); 
+//end add
+
 window.onload = function(){
 
     loadJsonData("assets/data/mountains.json").then((mountains) => {
         mountainsArray = mountains.mountains;
+
+            //added this
+        mountainsArray.forEach(function(mountain) {
+            mountainsDDL.innerHTML += `<option value="${mountain.name}">${mountain.name}</option>`
+        });
+
+        searchResults.classList.add("hidden") 
+            //end
+
+
     })
 
 }
--- assets/scripts/mountains.js
+++ assets/scripts/mountains.js
@@ -1,19 +1,39 @@
-let theDropDown = document.querySelector("#mountainDDL"); 
 let searchResults = document.querySelector("#searchResults");
 let searchResultsBody = document.querySelector("#searchResults tbody");
 
 
-window.onload = function() {
-    generateMountainsDDLOptions();
-  };
+mountainsDDL.addEventListener("change", function (event) {
+    //debugger
+    let mountain = event.target.value;
+
+    let filteredMountains = mountainsArray.filter((mountain) => {
+    return mountain === mountain // was return mountain.name
+    })
+
+    generateTableRows(filteredMountains)
+
+    searchResults.classList.remove("hidden")
+    console.log("change event detected")
+})
+
    
-    function generateMountainsDDLOptions(){
-        mountainsArray.forEach((mountain) => {
-            theDropDown.innerHTML += `<option value=${mountain}>${mountain}</option>`
-        })
-    }
+//Function that generates table when selecting a state from DDL 
+function generateTableRows(someArrayOfData){
 
+    //clear out the tables previous results
+    searchResultsBody.innerHTML = ""
 
-setTimeout (() => {
-console.log(mountainsArray)
-}, 5000 );
+    //generate new table rows and appewnd to the the tbody innerHTML
+
+someArrayOfData.forEach((mountain)=>{
+        let row = ""
+        row += `<tr>`
+        row += `    <td>${mountain.name}</td>`
+        row += `    <td>${mountain.elevation}</td>`
+        row += `    <td>${mountain.effort}</td>`
+        row += `</tr>`
+
+        searchResultsBody.innerHTML += row
+
+    })
+}
